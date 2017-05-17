f0e8e45105fbc2f1e60dc40ce982613c1c19b5df
Julie Xue <julie.xue11@gmail.com>
2017-05-17 13:52:52 -0400

    let go new tab

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 $('body').on('click', '*', function(event) {
-     window.alert(' ID of element- testing'); 
-    window.alert(' ID of element=' + $(this).attr('id'));  // Get ID attribute
-    window.alert(' ID of Parent element=' + $(this).parent().attr('id'));
+    window.alert(' ID of element- testing');
+    // window.alert(' ID of element=' + $(this).attr('id'));  // Get ID attribute
+    // window.alert(' ID of Parent element=' + $(this).parent().attr('id'));
+    // var newURL = "https://waterlooworks.uwaterloo.ca/myAccount/co-op/coop-postings.html";
+    var newURL = "https://google.com";
+    chrome.tabs.create({ url: newURL });
 });
